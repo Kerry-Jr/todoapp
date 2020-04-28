c99a71f9290bd7a9d101f23171ee33439a170ee5
Kerry Smith <kerrysfs@gmail.com>
2020-04-28 10:03:40 -0700

    more react

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './app.css'
+//functional components - another term is dumb component- no logic inside all they do is render HTML.
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <p>Hello World</p>
+//     </div>
+//   );
+// }
+//above code is the same as below code
+
+const name = 'Kerry';
+const lastname = 'Smith';
+const purpleMode = true;
+
+const RenderListItems = props => {
+  return props.items.map((item, index) => {
+    return <li key={index}>{item}</li>;
+  });
 }
+const Greetings = props => (
+  <div>
+    <h1>Hello World</h1>
+    <p>Hi my name is { purpleMode ? props.name : 'Kerry'}</p>
+    <p>{props.lastName}</p>
+    <ul>
+      <p>My favorite things to do in quarantine</p>
+      <RenderListItems items={["Play videogames", 'Sleep', 'Code']}/>
+    </ul>
+    <ul>
+      <p>My favorite movies to watch during the pandemic</p>
+      <RenderListItems items={["Plup fiction", 'John Wick', 'Harry Potter']}/>
+    </ul>
+  </div>
+);
+
+
+
+
+const App = props => 
+   (
+  <div className="colorBlue" style={{ fontSize: '40px', backgroundColor: purpleMode ? 'purple' : 'green'}}>
+ <Greetings name={props.name} lastname={props.lastname}/>
+{console.log(props)}
+  </div>
+  );
+
 
 export default App;
